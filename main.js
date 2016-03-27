4eb0334e61a184f4b5217cbd709e3d3a232c5bc3
seanmtracey <seanmtracey@gmail.com>
2016-03-27 13:51:02 +0100

    Added server, s3o authentication and endpoint to trigger the linear solenoid

--- main.js
+++ main.js
@@ -4,16 +4,31 @@ const s3o = require('s3o-middleware');
 const env = require('dotenv').config();
 const denodeify = require('denodeify');
 const dnsLookup = denodeify( require('dns').lookup );
+const express = require('express');
+var app = express();
 
-const localIP = dnsLookup(require('os').hostname())
+app.listen(process.env.PORT);
+
+const localAddress = dnsLookup(require('os').hostname())
 	.then( IP => {
 		const addressWithPort = `${IP}:${process.env.PORT}`;
 		console.log(addressWithPort);
-		eddystone.advertiseUrl(`http://${addressWithPort}`, {
+
+		eddystone.advertiseUrl(`http://${addressWithPort}/`, {
 			txPowerLevel: -22,
 			tlmCount: 2,
 			tlmPeriod: 10
 		});	
+
 		return addressWithPort;
 	})
+	.catch(err => {
+		console.log(err);
+	})
 ;
+
+app.use(s3o);
+
+app.get('/', function(req, res) {
+  res.send('You can haz tea.');
+});
