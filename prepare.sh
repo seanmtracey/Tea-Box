333088adf483ef7e42522c9a9c3eb02a80fb51ed
seanmtracey <seanmtracey@gmail.com>
2016-08-07 00:02:01 +0100

    Make that prepare.sh script work for *YOU*

--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,12 @@
-sudo apt-get install bluetooth bluez libbluetooth-dev libudev-dev
-sudo apt-get install -y build-essential
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+apt-get install bluetooth bluez libbluetooth-dev libudev-dev
+apt-get install -y build-essential
+service bluetooth stop
+update-rc.d bluetooth remove
+systemctl stop bluetooth
+systemctl disable bluetooth
+npm i --unsafe-perm
+reboot
